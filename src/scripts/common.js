0bf41d7a99af10cf286c4f9c051391fe71d0844c
sanjar <sanjar@avexdesigns.com>
2022-04-25 14:12:40 -0400

    s

--- src/scripts/common.js
+++ src/scripts/common.js
@@ -3,7 +3,6 @@ import Swiper, { Navigation, Pagination, Thumbs } from "swiper";
 
 import { configureCart } from "liquid-ajax-cart";
 console.log('assets in github actions')
-console.log('assets in github actions2')
 /**
  * Configure Liquid Ajax Cart
  */
