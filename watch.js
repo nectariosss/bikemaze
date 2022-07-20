60526db6827a1ef4e403f30c4885e0ba20606546
vecume <shahzod.bek200109@gmail.com>
2022-07-21 03:51:30 +0500

    Add liquid formatter

--- watch.js
+++ watch.js
@@ -1,41 +1,41 @@
-const fs = require("fs")
-const fsAsync = require("fs/promises")
+const fs = require('fs');
+const fsAsync = require('fs/promises');
 
-const COMPONENTS_PATH = __dirname + "/src/scripts/components"
-const SNIPPETS_PATH = __dirname + "/snippets"
-const mode = process.env.NODE_ENV || "development";
+const COMPONENTS_PATH = __dirname + '/src/scripts/components';
+const SNIPPETS_PATH = __dirname + '/snippets';
+const mode = process.env.NODE_ENV || 'development';
 
-populateComponents()
-mode === "development" && watch()
+populateComponents();
+mode === 'development' && watch();
 
 function watch() {
   fs.watch(COMPONENTS_PATH, async (eventType, filename) => {
-    if(eventType === "rename") {
-      await populateComponents()
+    if (eventType === 'rename') {
+      await populateComponents();
     }
-  })
+  });
 }
 
 async function populateComponents() {
   const files = await getComponents();
   const components = {};
-  
-  files.forEach(file => {
-    components[file] = getAssetUrl(file)
-  })
+
+  files.forEach((file) => {
+    components[file] = getAssetUrl(file);
+  });
 
   writeSnippet(components);
 
-  return components
+  return components;
 }
 
 async function getComponents() {
   const files = await fsAsync.readdir(COMPONENTS_PATH);
-  return files.map(file => file.replace(".js", ""))
+  return files.map((file) => file.replace('.js', ''));
 }
 
 function getAssetUrl(name) {
-  return `{{ 'component-${name}.min.js' | asset_url }}`
+  return `{{ 'component-${name}.min.js' | asset_url }}`;
 }
 
 function writeSnippet(components) {
@@ -43,9 +43,9 @@ function writeSnippet(components) {
 <script type="text/javascript">
   window.components = ${JSON.stringify(components, null, 2)}
 </script>
-  `
+  `;
   fs.writeFile(`${SNIPPETS_PATH}/components.liquid`, file, (err) => {
-    if(err) console.log(err)
-    else console.log("Components list is generated!")
-  })
+    if (err) console.log(err);
+    else console.log('Components list is generated!');
+  });
 }
